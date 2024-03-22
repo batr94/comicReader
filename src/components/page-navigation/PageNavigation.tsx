5f9b9bbe1f0a8866d89371718fdd2688847b2cd0
Батырбек Айгалиев <www.bat.94@gmail.com>
2024-03-22 15:32:24 +0500

    add pagination buttons for pages

--- src/components/page-navigation/PageNavigation.tsx
+++ src/components/page-navigation/PageNavigation.tsx
@@ -1,14 +1,16 @@
+import { FaCaretUp, FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import usePageInfo from '../../hooks/usePageInfo';
 import './pageNavigation.css';
 
 // https://react.dev/reference/react-dom/components/select
 const PageNavigation = () => {
-  const { pageList, currentPage, setPage } = usePageInfo();
+  const { pageList, currentPage, setPage, nextPage, previousPage } = usePageInfo();
 
   return (
     <nav className='page-navigation'>
       <label className='page-list'>
         <span className='page-list__label'>Page {currentPage + 1} / {pageList.length}</span>
+        <FaCaretUp />
         <select
           className='page-list__select'
           value={currentPage}
@@ -27,6 +29,20 @@ const PageNavigation = () => {
           )}
         </select>
       </label>
+      <div className='page-pagination'>
+        <button
+          className='page-pagination__item'
+          onClick={() => previousPage()}
+        >
+          <FaChevronLeft size={14} />
+        </button>
+        <button
+          className='page-pagination__item'
+          onClick={() => nextPage()}
+        >
+          <FaChevronRight size={14} />
+        </button>
+      </div>
     </nav>
   )
 }
